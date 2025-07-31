ce3c9fb87d4bb6c7c552e2264d49a737b0689b74
abc <123@example.com>
2025-07-31 09:48:21 +0800

    test

--- project-vue/commitlint.config.js
+++ project-vue/commitlint.config.js
@@ -1,19 +1,13 @@
-module.exports = {
-  // 继承 Angular 规范（最常用的规范）
-  extends: ['@commitlint/config-conventional'],
-};
-
 // module.exports = {
+//   // 继承 Angular 规范（最常用的规范）
 //   extends: ['@commitlint/config-conventional'],
-//   rules: {
-//     // 自定义允许的提交类型
-//     'type-enum': [
-//       2, // 错误级别：2 表示必须符合（1 是警告，0 是关闭）
-//       'always',
-//       ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert', 'wip']
-//       // 新增 'wip' 类型（表示工作中）
-//     ],
-//     // 允许 subject 首字母大写（默认不允许）
-//     'subject-case': [0]
-//   }
 // };
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // 自定义规则
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-min-length': [2, 'always', 3], // subject 最小长度为 3
+  },
+};
