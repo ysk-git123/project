8352744f5000e13abe6018f6b4e90b0be4bbc8d6
abc <123@example.com>
2025-08-02 18:56:38 +0800

    lzy_vue

--- project-vue/babel.config.js
+++ project-vue/babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-  presets: ['@vue/cli-plugin-babel/preset'],
-};
-=======
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
-}
->>>>>>> origin/main
+  ],
+};
--- project-vue/vue.config.js
+++ project-vue/vue.config.js
@@ -1,14 +1,8 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const path = require('path');
+const path = require('path')
 module.exports = {
-  // ... 其他原有配置
   chainWebpack: (config) => {
     config.resolve.set('extensions', ['.js', '.ts', '.vue', '.json']).set('alias', {
       '@': path.resolve(__dirname, 'src'),
-    });
+    })
   },
-};
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+}
