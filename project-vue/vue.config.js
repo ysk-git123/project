50172c1d32c438636e4d7e5365012d658fbd16e0
abc <123@example.com>
2025-08-03 17:44:11 +0800

    lzy

--- project-vue/vue.config.js
+++ project-vue/vue.config.js
@@ -1,8 +1,8 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
   chainWebpack: (config) => {
     config.resolve.set('extensions', ['.js', '.ts', '.vue', '.json']).set('alias', {
       '@': path.resolve(__dirname, 'src'),
-    })
+    });
   },
-}
+};
